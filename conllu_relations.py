ad89701788ee168c39fecd7d16ef3743b662fd11
Andrey Verigin <andrei-verigin@mail.ru>
2019-02-24 19:29:06 +0300

    Add methods to represent relations by strings

--- conllu_relations.py
+++ conllu_relations.py
@@ -14,6 +14,18 @@ class SentenceRelations:
         self.sentence = sentence
         self._extract_relations()
 
+    def relations_as_strings_tuples(self):
+        res = []
+        for relation in self.relations:
+            res.append(self.relation_to_strings_tuple(relation))
+        return res
+
+    def relation_to_strings_tuple(self, relation):
+        left = self._word_to_string(relation[0])
+        center = self._word_to_string(relation[1])
+        right = self._word_to_string(relation[2])
+        return (left, center, right)
+
     def _extract_relations(self):
         self._extract_verb_relations()
 
@@ -23,7 +35,6 @@ class SentenceRelations:
         all_objects = [self._get_objects(verb) for verb in verbs]
         all_oblique_nominals = [self._get_oblique_nominals(verb) for verb in verbs]
         for i, verb in enumerate(verbs):
-            print(verb.form)
             verb_subjects = all_subjects[i]
             verb_objects = all_objects[i]
             verb_oblique_nominals = all_oblique_nominals[i]
@@ -40,11 +51,27 @@ class SentenceRelations:
                 for obl in verb_oblique_nominals:
                     self.relations.append((subj, verb, obl))
 
+    def _word_to_string(self, word):
+        prefix = self._get_string_prefix(word)
+        return prefix + word.form
+
+    def _get_string_prefix(self, word):
+        res = ""
+        for child_idx in word.children:
+            child = self.sentence.words[child_idx]
+            if child.deprel == "case":
+                res += child.form + " "
+            elif child.deprel == "aux":
+                res += child.form + " "
+            elif child.upostag == "PART":
+                res += child.form + " "
+        return res
+
     def _get_subjects(self, word):
         subj_list = []
         for child_idx in word.children:
             child = self.sentence.words[child_idx]
-            if child.deprel in ["nsubj", "nsubj:pass"]:
+            if self._is_subject(child):
                 subj_list.append(child)
                 subj_list += self._get_conjuncts(child)
         return subj_list
@@ -53,7 +80,7 @@ class SentenceRelations:
         obj_list = []
         for child_idx in word.children:
             child = self.sentence.words[child_idx]
-            if child.deprel in ["obj", "iobj"]:
+            if self._is_object(child):
                 obj_list.append(child)
                 obj_list += self._get_conjuncts(child)
         return obj_list
@@ -62,7 +89,7 @@ class SentenceRelations:
         obl_list = []
         for child_idx in word.children:
             child = self.sentence.words[child_idx]
-            if child.deprel in ["obl", "obl:agent"]:
+            if self._is_oblique_nominal(child):
                 obl_list.append(child)
                 obl_list += self._get_conjuncts(child)
         return obl_list
@@ -71,9 +98,21 @@ class SentenceRelations:
         conjuncts = []
         for child_idx in word.children:
             child = self.sentence.words[child_idx]
-            if child.deprel == "conj":
+            if self._is_conjunct(child):
                 conjuncts.append(child)
         return conjuncts
+
+    def _is_subject(self, word):
+        return word.deprel in ["nsubj", "nsubj:pass"]
+
+    def _is_object(self, word):
+        return word.deprel in ["obj", "iobj"]
+
+    def _is_oblique_nominal(self, word):
+        return word.deprel in ["obl", "obl:agent"]
+
+    def _is_conjunct(self, word):
+        return word.deprel == "conj"
 
 
 def get_case(sentence, word):
@@ -194,13 +233,12 @@ def get_relations(sentence):
 def simple_test(model):
     relations = []
     sentences = model.tokenize(
-        "Андрей пошел в магазин и аптеку, купил куртку и телефон, на улице становилось темно. Никита бегал в парке, а Андрей, Дима и Федор варили и жарили обед. Андрей, пока собирался на работу, съел завтрак."
+        "Андрей пошел в магазин и аптеку, купил куртку и телефон, на улице становилось темно. Никита определенно не будет бегать в парке, а Андрей, Дима и Федор варили и жарили обед. Андрей, пока собирался на работу, съел завтрак."
     )
     for s in sentences:
         model.tag(s)
         model.parse(s)
         relations = SentenceRelations(s)
-
     conllu = model.write(sentences, "conllu")
     with open("output.conllu", "w", encoding="utf8") as f:
         f.write(conllu)
