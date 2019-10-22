1628fe7a3a52d4096657481231fd6a04fc9a6450
Andrey Verigin <andrei-verigin@mail.ru>
2019-10-22 22:52:14 +0300

    Add support for missing attvalues tag

--- fix_gexf.py
+++ fix_gexf.py
@@ -1,6 +1,7 @@
 import argparse
 import xml.etree.ElementTree as ET
 
+
 def fix_gexf(root_element):
     graph_node = root_element.find("{http://www.gexf.net/1.1draft}graph")
     attributes_nodes = graph_node.findall("{http://www.gexf.net/1.1draft}attributes")
@@ -19,19 +20,24 @@ def fix_gexf(root_element):
                 node_attributes[attr_id] = attr_title
     nodes_node = graph_node.find("{http://www.gexf.net/1.1draft}nodes")
     for node_node in nodes_node.findall("{http://www.gexf.net/1.1draft}node"):
-        for attvalue_node in node_node.find(
-            "{http://www.gexf.net/1.1draft}attvalues"
-        ).findall("{http://www.gexf.net/1.1draft}attvalue"):
-            for_value = attvalue_node.get("for")
-            attvalue_node.set("for", node_attributes[for_value])
+        attvalues_node = node_node.find("{http://www.gexf.net/1.1draft}attvalues")
+        if attvalues_node is not None:
+            for attvalue_node in attvalues_node.findall(
+                "{http://www.gexf.net/1.1draft}attvalue"
+            ):
+                for_value = attvalue_node.get("for")
+                attvalue_node.set("for", node_attributes[for_value])
     edges_node = graph_node.find("{http://www.gexf.net/1.1draft}edges")
     for edge_node in edges_node.findall("{http://www.gexf.net/1.1draft}edge"):
-        for attvalue_node in edge_node.find(
-            "{http://www.gexf.net/1.1draft}attvalues"
-        ).findall("{http://www.gexf.net/1.1draft}attvalue"):
-            for_value = attvalue_node.get("for")
-            attvalue_node.set("for", edge_attributes[for_value])
+        attvalues_node = edge_node.find("{http://www.gexf.net/1.1draft}attvalues")
+        if attvalues_node is not None:
+            for attvalue_node in attvalues_node.findall(
+                "{http://www.gexf.net/1.1draft}attvalue"
+            ):
+                for_value = attvalue_node.get("for")
+                attvalue_node.set("for", edge_attributes[for_value])
     return root_element
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
