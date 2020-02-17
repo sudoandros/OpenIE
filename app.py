3657f504bf6b6a0fd57cde89b5c3de87ed35e0a1
sudoandros <andrei-verigin@mail.ru>
2020-02-17 23:29:51 +0300

    Add primitive Flask application

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+from udpipe_model import UDPipeModel
+from udpipe_parse_folder import parse_text
+
+app = Flask(__name__)
+UDPIPE_MODEL = UDPipeModel(r"models\russian-syntagrus-ud-2.4-190531.udpipe")
+
+
+@app.route("/")
+def root():
+    return "Hello"
+
+
+@app.route("/parse", methods=["POST"])
+def parse():
+    text = request.form["text"]
+    return parse_text(text, UDPIPE_MODEL)
