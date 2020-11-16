38de2976fc73c289d6442e77cd1a3b73be7e8dff
sudoandros <andrei-verigin@mail.ru>
2020-11-16 21:10:41 +0300

    Enable debug mode in Flask application

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def download(directory, filename):
 
 
 if __name__ == "__main__":
-    app.run(host=app.config["HOST"], port=app.config["PORT"])
+    app.run(debug=True, host=app.config["HOST"], port=app.config["PORT"])
