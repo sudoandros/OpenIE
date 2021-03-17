9c970474bef0748b951996c9585d7231fdfa390c
sudoandros <andrei-verigin@mail.ru>
2021-03-17 23:53:23 +0300

    Fix building script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-pyinstaller app.py --onefile --clean --hidden-import \
-scipy.special.cython_special --hidden-import sklearn.neighbors._typedefs \
---hidden-import sklearn.utils._cython_blas --hidden-import \
-sklearn.neighbors._quad_tree --hidden-import sklearn.tree._utils
+pyinstaller app.py --onefile --clean \
+--hidden-import scipy.special.cython_special \
+--hidden-import sklearn.neighbors._typedefs \
+--hidden-import sklearn.utils._cython_blas \
+--hidden-import sklearn.neighbors._quad_tree \
+--hidden-import sklearn.tree._utils \
+--hidden-import scipy.spatial.transform._rotation_groups
