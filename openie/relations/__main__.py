a3b8536421c8d26f8f905d1275d90c79ee24133e
sudoandros <andrei-verigin@mail.ru>
2022-09-11 16:50:49 +0300

    Use `typer` for `relation` module's CLI

--- openie/relations/__main__.py
+++ openie/relations/__main__.py
@@ -1,10 +1,10 @@
-import argparse
 import json
 import logging
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 import gensim.downloader
+import typer
 from tqdm import tqdm
 
 from .text import TextReltuples
@@ -36,34 +36,32 @@ def build_dir_graph(
     print(text_reltuples.graph.nodes_number, text_reltuples.graph.edges_number)
 
 
-if __name__ == "__main__":
+def main(
+    conllu_dir: Path = typer.Argument(
+        ..., help="Path to the directory containing parsed text in conllu format"
+    ),
+    save_dir: Path = typer.Argument(
+        ..., help="Path to the directory to save relations to"
+    ),
+    add: bool = typer.Option(False, help="Include additional relations"),
+    entities_limit: Optional[float] = typer.Option(
+        None, help="Filter extracted relations to only contain this many entities"
+    ),
+):
     logging.basicConfig(
         handlers=[logging.FileHandler("logs/server.log", "a", "utf-8")],
         level=logging.INFO,
         format="%(asctime)s | %(levelname)s | %(message)s",
     )
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "conllu_dir",
-        help="Path to the directory containing parsed text in conllu format",
-    )
-    parser.add_argument("save_dir", help="Path to the directory to save relations to")
-    parser.add_argument(
-        "--add", help="Include additional relations", action="store_true"
-    )
-    parser.add_argument(
-        "--entities-limit",
-        help="Filter extracted relations to only contain this many entities",
-        type=int,
-    )
-    args = parser.parse_args()
-    conllu_dir = Path(args.conllu_dir)
-    save_dir = Path(args.save_dir)
-    entities_limit = args.entities_limit or float("inf")
+    entities_limit = entities_limit or float("inf")
     with open("stopwords.txt", mode="r", encoding="utf-8") as file:
-        stopwords = list(file.read().split())
+        stopwords = [line.strip() for line in file.readlines()]
     w2v_model = gensim.downloader.load("word2vec-ruscorpora-300")
 
     build_dir_graph(
-        conllu_dir, save_dir, stopwords, args.add, entities_limit, w2v_model,
+        conllu_dir, save_dir, stopwords, add, entities_limit, w2v_model,
     )
+
+
+if __name__ == "__main__":
+    typer.run(main)
